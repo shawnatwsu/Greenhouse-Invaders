c72aff1f873999f8bbf5b0432f11d87a784e5d38
Master Shimey <shimeyloo@gmail.com>
2021-09-18 22:19:29 -0700

    Added theme song

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ GUN_X = 440
 GUN_Y =600
 WIN_KILLS = 5  # Has to be a multiple of 5
 
+
 class CO2Invaders:
     def __init__(self):
         self._init_pygame()
@@ -37,6 +38,9 @@ class CO2Invaders:
         self.leaf = LeafBullet(self.screen, self.gun.gunX+30, "ready", LEAF_SPEED) 
 
     def main_loop(self):
+        mixer.music.load("sounds/themeSong.mp3")
+        mixer.music.play(-1)
+
         running = True
         while running:
             self._handle_input()
@@ -170,7 +174,7 @@ class CO2Invaders:
 
         # Background gets cleaner as enemies are killed
         clear_per_kills = WIN_KILLS // 5
-        if self.clear_kills == clear_per_kills:
+        if self.clear_kills == clear_per_kills and self._state == "unfinished":
             background_arr = [
                 pygame.image.load('images/game_2.jpg'),
                 pygame.image.load('images/game_3.jpg'),
