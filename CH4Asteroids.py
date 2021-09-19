adb4dcc960a836993a54a2f4bb2a60f606ba01cd
theharkraven <72245657+theharkraven@users.noreply.github.com>
2021-09-18 22:22:56 -0700

    Ch4(methane) code

---
+++ CH4Asteroids.py
@@ -0,0 +1,36 @@
+import pygame
+import random
+
+
+class CH4Asteroids:
+
+    def __init__(self, screen):
+        self.screen = screen
+        self.image = pygame.image.load('images/Ch4_sprite.png')
+
+        self._x = random.randint(0, 800)
+        self._y = random.randint(50, 150)
+
+        self._x_change = 25
+        self._y_change = 25
+        self._num_of_particles = 1
+
+    def move(self, screen_size: tuple):
+        """ defines movement of the CO2 'astroids' """
+
+        # if particle hits the left side of screen, change its direction
+        # also move the particle down some y-value
+        if self._x <= 0:
+            self._x_change = 3
+            self._y += self._y_change
+        # if particle hits the right side of screen, change its direction
+        # also move the particle down some y-value
+        elif self._x >= screen_size[0]:
+            self._x_change = -3
+            self._y += self._y_change
+
+        self._x += self._x_change
+
+    def draw(self, surface):
+        blit_position = (self._x, self._y)
+        surface.blit(self.image, blit_position)
